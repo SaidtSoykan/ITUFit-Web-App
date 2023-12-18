de63b4396bc9242cc29715690a2832f68da80138
erdoganer19 <73936365+ituitis20-erdoganer19@users.noreply.github.com>
2023-12-18 18:26:10 +0300

    Agenda.js

--- src/components/Agenda.js
+++ src/components/Agenda.js
@@ -1,6 +1,7 @@
 // src/components/TimeSlots.js
 
-import React from 'react';
+import React, { useState } from 'react';
+import './Agenda.css';
 
 const TimeSlots = ({ day, handleSlotClick }) => {
   const generateTimeSlots = () => {
@@ -8,26 +9,44 @@ const TimeSlots = ({ day, handleSlotClick }) => {
     for (let i = 8; i <= 18; i += 2) {
       timeSlots.push({
         slot: `${i}:00 - ${i + 2}:00`,
-        isReserved: false, // Varsa true olarak işaretleyin
+        isReserved: false,
       });
     }
     return timeSlots;
   };
 
+  const [selectedSlot, setSelectedSlot] = useState(null);
+  const [timeSlots, setTimeSlots] = useState(generateTimeSlots());
+
+  const handleReserveClick = () => {
+    if (selectedSlot) {
+      const updatedTimeSlots = timeSlots.map((timeSlot) =>
+        timeSlot.slot === selectedSlot ? { ...timeSlot, isReserved: true } : timeSlot
+      );
+      setTimeSlots(updatedTimeSlots);
+    }
+  };
+
   return (
-    <div className="time-slots">
+    <div className="time-slots-card">
       <h4>Time Slots for {day}:</h4>
-      <ul>
-        {generateTimeSlots().map((timeSlot, index) => (
-          <li
+      <div className="time-slots-container">
+        {timeSlots.map((timeSlot, index) => (
+          <div
             key={index}
-            onClick={() => handleSlotClick(day, timeSlot.slot)}
-            className={timeSlot.isReserved ? 'reserved' : ''}
+            onClick={() => setSelectedSlot(timeSlot.slot)}
+            className={`time-slot-card ${timeSlot.isReserved ? 'reserved' : ''} ${selectedSlot === timeSlot.slot ? 'selected' : ''}`}
           >
+            {timeSlot.isReserved && <span className="full-tag">(full)</span>}
             {timeSlot.slot}
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
+      {selectedSlot && !timeSlots.find((timeSlot) => timeSlot.slot === selectedSlot).isReserved && (
+        <button className="reserve-button" onClick={handleReserveClick}>
+          Reserve {selectedSlot}
+        </button>
+      )}
     </div>
   );
 };
